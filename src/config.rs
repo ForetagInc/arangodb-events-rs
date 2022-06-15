b489edf9c0fdad90299b9a3ab9bd8ac2d174df6c
itsezc <chirub@foretag.co>
2022-06-15 23:57:47 +0100

    fix: `TriggerConfig` optional args for new methods

--- src/config.rs
+++ src/config.rs
@@ -13,23 +13,23 @@ impl TriggerConfig<'_> {
 		}
 	}
 
-	pub fn new_with_events(collection: &str, events: Option<Vec<&'static str>>) -> Self {
+	pub fn new_with_events(collection: &str, events: Vec<&'static str>) -> Self {
 		Self {
 			collection: collection.to_string(),
-			events,
+			events: Some(events),
 			keys: None,
 		}
 	}
 
 	pub fn new_with_events_and_keys(
 		collection: &str,
-		events: Option<Vec<&'static str>>,
-		keys: Option<Vec<&'static str>>,
+		events: Vec<&'static str>,
+		keys: Vec<&'static str>,
 	) -> Self {
 		Self {
 			collection: collection.to_string(),
-			events,
-			keys,
+			events: Some(events),
+			keys: Some(keys),
 		}
 	}
 }
